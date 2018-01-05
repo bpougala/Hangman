a88b584aeb97e52fc7e1f106784e6d6df94e1e21
Biko Pougala <bpougala@gmail.com>
2018-01-05 12:27:29 +0000

    first commit

---
+++ hangman/main/Timer.java
@@ -0,0 +1,36 @@
+package main;
+
+
+/**
+ * Write a description of class Timer here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Timer
+{
+
+    private int seconds;
+    private static final int unit = 60; // unit of time 
+
+    /**
+     * Constructor for objects of class Timer
+     */
+    public Timer(int seconds)
+    {
+        // initialise instance variables
+        this.seconds = seconds; 
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int wait(int seconds)
+    {
+        // not implemented yet
+        return 0;
+    }
+}
